264ae94a10c92df0e30cb46a06013e98df738637
Chris-P1 <cpicken@virginmedia.com>
2017-08-28 17:31:05 +0100

    Update ltc_scrape_strip.py

--- ltc_scrape_strip.py
+++ ltc_scrape_strip.py
@@ -7,21 +7,17 @@ while True:
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-
     page = requests.get('http://www.litecoinrate.co.uk')
     tree = html.fromstring(page.content)
     price = tree.xpath('//*[@id="tablepress-1"]/tbody/tr[1]/td/strong/text()')
 
-
     string = price[0]
     new_str = re.sub('[^a-zA-Z0-9\n\.]', ' ', string)
-
 
     fh = open("ltcscraped.txt", "a")
     scraped_and_stripped_text = new_str
     fh.write(scraped_and_stripped_text + ' ')
     fh.close()
-
 
     time.sleep(3600)
 
